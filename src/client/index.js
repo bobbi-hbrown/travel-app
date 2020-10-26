1d3b730e9acfb95dd83442ec95b4790eac119c3d
bobbihb <rholdingbrown@gmail.com>
2020-10-26 16:56:21 +0000

    fix check for null values in local storage

--- src/client/index.js
+++ src/client/index.js
@@ -14,9 +14,8 @@ import {updateUI} from "./js/updateUI";
 
 // check to see if data has previously been fetched and saved upon refresh
 let items
-if (localStorage['items'] !== "undefined") {
+if (localStorage["items"] != "undefined" && localStorage["items"] != "null") {
     items = localStorage.getItem('items');
-    console.log(items, typeof items);
     updateUI(items)
 } else {
     items = []
